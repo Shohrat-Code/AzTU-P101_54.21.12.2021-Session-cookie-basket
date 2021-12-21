8482f145005c72cde300000e4c15e8a7e04593bf
Shohrat-Code <shohrat@code.edu.az>
2021-12-21 13:53:20 +0400

    Codes

---
+++ OneHealth/OneHealth/Controllers/NewsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneHealth.Data;
+using OneHealth.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneHealth.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public NewsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            VmHome model = new VmHome();
+            model.Setting = _context.Settings.FirstOrDefault();
+            model.Socials = _context.Socials.ToList();
+
+            return View(model);
+        }
+    }
+}
---
+++ OneHealth/OneHealth/ViewModels/VmHome.cs
@@ -0,0 +1,15 @@
+﻿using OneHealth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneHealth.ViewModels
+{
+    public class VmHome : VmLayout
+    {
+        public List<Service> Services { get; set; }
+        public List<Doctor> Doctors { get; set; }
+        public List<Blog> Blogs { get; set; }
+    }
+}
